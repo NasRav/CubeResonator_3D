f29f5b8920051c88413d8b446dac3f05a3df499a
Ravil Nasyrov <42833143+NasRav@users.noreply.github.com>
2022-08-25 09:26:59 +0300

    energy solver added

--- CubeResonator_3D/Explicit.cpp
+++ CubeResonator_3D/Explicit.cpp
@@ -5,24 +5,24 @@ Explicit::Explicit(double X, double Y, double Z, double T, double l, int Nx, int
 	_dx(_X / (_Nx - 1)), _dy(_Y / (_Ny - 1)), _dz(_Z / (_Nz - 1)),
 	_Pr(abs(_mu / _ro0 / (1.302e-10 * pow(_T, 2) + 5.893e-6 * _T - 7283e-6)))
 {
-	_ro.resize(_Nx);
+//	_ro.resize(_Nx);
 	_u.resize(_Nx);
 	_v.resize(_Nx);
 	_w.resize(_Nx);
 	_e.resize(_Nx);
-	d_ro.resize(_Nx);
+//	d_ro.resize(_Nx);
 	d_u.resize(_Nx);
 	d_v.resize(_Nx);
 	d_w.resize(_Nx);
 	d_e.resize(_Nx);
 	for (int i = 0; i < _Nx; i++)
 	{
-		_ro[i].resize(_Ny);
+//		_ro[i].resize(_Ny);
 		_u[i].resize(_Ny);
 		_v[i].resize(_Ny);
 		_w[i].resize(_Ny);
 		_e[i].resize(_Ny);
-		d_ro[i].resize(_Ny);
+//		d_ro[i].resize(_Ny);
 		d_u[i].resize(_Ny);
 		d_v[i].resize(_Ny);
 		d_w[i].resize(_Ny);
@@ -31,12 +31,12 @@ Explicit::Explicit(double X, double Y, double Z, double T, double l, int Nx, int
 	for (int i = 0; i < _Nx; i++)
 		for (int j = 0; j < _Ny; j++)
 		{
-			_ro[i][j].resize(_Nz);
+//			_ro[i][j].resize(_Nz);
 			_u[i][j].resize(_Nz);
 			_v[i][j].resize(_Nz);
 			_w[i][j].resize(_Nz);
 			_e[i][j].resize(_Nz);
-			d_ro[i][j].resize(_Nz);
+//			d_ro[i][j].resize(_Nz);
 			d_u[i][j].resize(_Nz);
 			d_v[i][j].resize(_Nz);
 			d_w[i][j].resize(_Nz);
@@ -55,7 +55,31 @@ void Explicit::calculate_dt()
 	if (_Pr / (6 * _gamma) < 0.15)
 		this->_dt = _Pr / (6 * _gamma) * _ro0 * pow(d_min, 2) / _mu;
 	else
-		this->_dt = 0.15 * _ro0 * pow(_dx, 2) / _mu;
+		this->_dt = 0.15 * _ro0 * pow(d_min, 2) / _mu;
+}
+
+double Explicit::d_dx(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k)
+{
+	return (f[i][j][k] - f[i - 1][j][k]) / _dx;
+}
+
+double Explicit::d_dy(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k)
+{
+	return (f[i][j][k] - f[i][j - 1][k]) / _dy;
+}
+
+double Explicit::d_dz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k)
+{
+	return (f[i][j][k] - f[i][j][k - 1]) / _dz;
+}
+
+double Explicit::H(int i, int j, int k)
+{
+	return 2 * ( pow(d_dx(_u, i, j, k), 2) + pow(d_dy(_v, i, j, k), 2) + pow(d_dz(_w, i, j, k), 2)
+		+ 0.5 * pow(d_dy(_u, i, j, k) + d_dx(_v, i, j, k), 2)
+		+ 0.5 * pow(d_dz(_v, i, j, k) + d_dy(_w, i, j, k), 2)
+		+ 0.5 * pow(d_dx(_w, i, j, k) + d_dz(_u, i, j, k), 2) )
+		- 2 / 3 * pow(d_dx(_u, i, j, k) + d_dy(_v, i, j, k) + d_dz(_w, i, j, k), 2);
 }
 
 void Explicit::calculate_dU()
@@ -73,8 +97,8 @@ void Explicit::calculate_dU()
 				d_w[i][j][k] = _mu * _dt / (_ro0 * _dx * _dy)
 					* (4 / 3 * L_zz(_w, i, j, k) + 1 / 3 * L_xz(_u, i, j, k) + 1 / 3 * L_yz(_v, i, j, k)
 						+ L_xx(_w, i, j, k) + L_yy(_w, i, j, k));
-//				d_e[i][j][k] = _mu * _dt / (_ro0 * _dx * _dy) * _gamma / _Pr
-//					* (L_xx(_e, i, j, k) + L_yy(_e, i, j, k) + L_zz(_e, i, j, k) + H(i, j, k));
+				d_e[i][j][k] = _mu * _dt / (_ro0 * _dx * _dy) * _gamma / _Pr
+					* (L_xx(_e, i, j, k) + L_yy(_e, i, j, k) + L_zz(_e, i, j, k) + H(i, j, k));
 			}
 }
 
@@ -87,21 +111,11 @@ void Explicit::calculate_U(unsigned long it)
 				_u[i][j][k] += d_u[i][j][k];
 				_v[i][j][k] += d_v[i][j][k];
 				_w[i][j][k] += d_w[i][j][k];
-//				_e[i][j][k] += d_e[i][j][k];
+				_e[i][j][k] += d_e[i][j][k];
 			}
 	for (int i = 1; i < _Nx - 1; i++)
 		for (int j = 1; j < _Ny - 1; j++)
 			_u[i][j][0] = _l * _omega * sin(_omega * _dt * it);
-}
-
-double Explicit::get_Pr()
-{
-	return this->_Pr;
-}
-
-double Explicit::get_dt()
-{
-	return this->_dt;
 }
 
 double Explicit::L_xx(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k)
@@ -132,4 +146,14 @@ double Explicit::L_xz(std::vector<std::vector<std::vector<double>>> f, int i, in
 double Explicit::L_yz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k)
 {
 	return 0.25 * (f[i][j + 1][k + 1] - f[i][j + 1][k - 1] - f[i][j - 1][k + 1] + f[i][j - 1][k - 1]);
+}
+
+double Explicit::get_Pr()
+{
+	return this->_Pr;
+}
+
+double Explicit::get_dt()
+{
+	return this->_dt;
 }
--- CubeResonator_3D/Explicit.h
+++ CubeResonator_3D/Explicit.h
@@ -8,13 +8,19 @@ class Explicit : public Resonator
 public:
 	Explicit(double X, double Y, double Z, double T, double l, int Nx, int Ny, int Nz);
 
+	void	calculate_dt();
+
 	double	L_xx(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
 	double	L_yy(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
 	double	L_zz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
 	double	L_xy(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
 	double	L_xz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
 	double	L_yz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
-	void	calculate_dt();
+	double	d_dx(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
+	double	d_dy(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
+	double	d_dz(std::vector<std::vector<std::vector<double>>> f, int i, int j, int k);
+	double	H(int i, int j, int k);
+
 	void	calculate_dU();
 	void	calculate_U(unsigned long it);
 	double	get_Pr();
@@ -24,12 +30,12 @@ private:
 	const int		_Nx, _Ny, _Nz;
 	const double	_dx, _dy, _dz, _Pr;
 	double			_dt;
-	std::vector<std::vector<std::vector<double>>>	_ro;
+//	std::vector<std::vector<std::vector<double>>>	_ro;
 	std::vector<std::vector<std::vector<double>>>	_u;
 	std::vector<std::vector<std::vector<double>>>	_v;
 	std::vector<std::vector<std::vector<double>>>	_w;
 	std::vector<std::vector<std::vector<double>>>	_e;
-	std::vector<std::vector<std::vector<double>>>	d_ro;
+//	std::vector<std::vector<std::vector<double>>>	d_ro;
 	std::vector<std::vector<std::vector<double>>>	d_u;
 	std::vector<std::vector<std::vector<double>>>	d_v;
 	std::vector<std::vector<std::vector<double>>>	d_w;
